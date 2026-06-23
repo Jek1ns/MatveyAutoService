21625c2971ff5ccbcff8c9c58fd22db2f9342c11
jek1ns <evgeniykukut@gmail.com>
2026-06-23 22:18:12 +0200

    Cоздан главный экран

--- app/src/main/java/com/example/autoservice/MainActivity.java
+++ app/src/main/java/com/example/autoservice/MainActivity.java
@@ -1,24 +1,38 @@
 package com.example.autoservice;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int id = item.getItemId();
+
+            if (id == R.id.nav_home) selectedFragment = new HomeFragment();
+            else if (id == R.id.nav_appointment) selectedFragment = new AppointmentFragment();
+            else if (id == R.id.nav_shop) selectedFragment = new ShopFragment();
+
+            if (selectedFragment != null) {
+                // Анимация смены фрагментов
+                getSupportFragmentManager().beginTransaction()
+                        .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+            return true;
         });
+
+        // Открываем первый фрагмент по умолчанию
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+        }
     }
 }
